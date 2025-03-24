c4f9c839a7cb5a352574f30ee34c94f1f579083e
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-03-24 01:50:56 -0300

    feat: adiciona Interface com comportamentos que serão usados pelo service

---
+++ src/OrganizeJobs.Application/Service/IEmpresaServices.cs
@@ -0,0 +1,13 @@
+﻿using OrganizeJobs.Application.Dto.Request;
+using OrganizeJobs.Application.Dto.Response;
+
+namespace OrganizeJobs.Application.Service;
+
+public interface IEmpresaServices
+{
+    Task<EmpresaResponseDto[]> ObterTodasEmpresas();
+    Task<EmpresaResponseDto> ObterEmpresaPorId(Guid id);
+    Task<EmpresaResponseDto> CraiarEmpresa(EmpresaRequestDto empresa);
+    Task<EmpresaResponseDto> AtualizarEmpresa(EmpresaRequestDto empresa);
+    Task<bool> DeletarEmpresa(Guid id);
+}
