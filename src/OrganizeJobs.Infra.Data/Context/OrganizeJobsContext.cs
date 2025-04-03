e0fb98677462fcbe52dd47a20d688aae4e9ee12b
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-04-03 00:37:26 -0300

    feat: adiciona Historico de Atividades

--- src/OrganizeJobs.Infra.Data/Context/OrganizeJobsContext.cs
+++ src/OrganizeJobs.Infra.Data/Context/OrganizeJobsContext.cs
@@ -8,6 +8,7 @@ public class OrganizeJobsContext : DbContext
     public DbSet<Empresa> Empresas { get; set; }
     public DbSet<Projeto> Projetos { get; set; }
     public DbSet<Tarefa> Tarefas { get; set; }
+    public DbSet<HistoricoAtividade> HistoricoAtividades { get; set; }
 
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
