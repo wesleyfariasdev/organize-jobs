3f614408d0ce30bf58e50171215d48d254fd0312
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-05-03 04:07:43 -0300

    adiciona dto para request de perfil e historico de atividades

---
+++ src/OrganizeJobs.Application/Dto/Request/PerfilRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace OrganizeJobs.Application.Dto.Request;
+
+public sealed record PerfilRequestDto(
+    string NomeCompleto,
+    string PerfilUrlImage,
+    decimal SalarioBruto,
+    bool SalarioSincronizado
+);
