a466d3b82eb3d7222f72b304fe48071959693a89
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-05-03 04:08:26 -0300

    adiciona mapeamento entre domain e dto

---
+++ src/OrganizeJobs.Application/ProfileMapping/ProfileMap.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using OrganizeJobs.Application.Dto.Request;
+using OrganizeJobs.Application.Dto.Response;
+using OrganizeJobs.Domain.Models;
+
+namespace OrganizeJobs.Application.ProfileMapping;
+
+public class ProfileMap : Profile
+{
+    public ProfileMap()
+    {
+        CreateMap<EmpresaRequestDto, Empresa>().ReverseMap();
+        CreateMap<EmpresaResponseDto, Empresa>().ReverseMap();
+
+        CreateMap<HistoricoAtividadeResquestDto, HistoricoAtividade>().ReverseMap();
+        CreateMap<HistoricoAtividadeResponseDto, HistoricoAtividade>().ReverseMap();
+
+        CreateMap<ProjetoRequestDto, Projeto>().ReverseMap();
+        CreateMap<ProjetoResponseDto, Projeto>().ReverseMap();
+
+        CreateMap<TarefaRequestDto, Tarefa>().ReverseMap();
+        CreateMap<TarefaResponseDto, Tarefa>().ReverseMap();
+
+        CreateMap<PerfilRequestDto, Perfil>().ReverseMap();
+        CreateMap<PerfilResponseDto, Perfil>().ReverseMap();
+    }
+}
