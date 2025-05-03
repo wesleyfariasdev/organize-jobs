8d94b2e6d3237bacad7ec021b5f5ec3762fa8ff3
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-05-03 04:15:32 -0300

    adiciona inversão de dependencia

--- src/OrganizeJobs.Infra.IoC/Services/ServiceConfig.cs
+++ src/OrganizeJobs.Infra.IoC/Services/ServiceConfig.cs
@@ -1,11 +1,29 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using OrganizeJobs.Application.ProfileMapping;
+using OrganizeJobs.Application.Service;
+using OrganizeJobs.Application.Service.IServices;
+using OrganizeJobs.Domain.Interface;
+using OrganizeJobs.Infra.Data.Repository;
 
 namespace OrganizeJobs.Infra.IoC.Services;
 
-internal static class ServiceConfig
+public static class ServiceConfig
 {
     public static IServiceCollection AddService(this IServiceCollection service)
     {
+        service.AddScoped<IEmpresaRepository, EmpresaRepository>();
+        service.AddScoped<IHistoricoAtividadeRespository, HistoricoAtividadeRepository>();
+        service.AddScoped<IProjetoRepository, ProjetoRepository>();
+        service.AddScoped<ITarefaRepository, TarefaRepository>();
+
+        service.AddScoped<IEmpresaServices, EmpresaServices>();
+        service.AddScoped<IHistoricoAtividadeServices, HistoricoAtividadeService>();
+        service.AddScoped<IPerfilServices, PerfilServices>();
+        service.AddScoped<IProjetoServices, ProjetoServices>();
+        service.AddScoped<ITarefaServices, TarefaServices>();
+
+        service.AddAutoMapper(typeof(ProfileMap));
+
         return service;
     }
 }
