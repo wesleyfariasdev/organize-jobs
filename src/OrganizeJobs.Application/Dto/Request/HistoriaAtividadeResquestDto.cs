fa0f2135b25ab451ae9f8ab393982a80161457f4
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-05-03 04:01:37 -0300

    remove arquivos não usados

--- src/OrganizeJobs.Application/Dto/Request/HistoriaAtividadeResquestDto.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace OrganizeJobs.Application.Dto.Request;
-
-public sealed record HistoriaAtividadeResquestDto(
-                        DateTime InicioAtividad,
-                        DateTime? FimAtividade,
-                        Guid TarefaId);
